3a22a6d04a9591d4ee38b6d5bb1a0a3a82a746bd
EL MANSOUR MAHMOUD <65395060+Mahmuud212@users.noreply.github.com>
2026-03-08 22:04:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("Dashboard IA - Satisfaction Client")
 
 st.write("Bienvenue dans le dashboard du projet SMC Insight.")
 
+# Simulation de données
+data = {
+    "Sentiment": ["Positif", "Neutre", "Négatif"],
+    "Nombre": [320, 150, 80]
+}
+
+df = pd.DataFrame(data)
+
+# KPI
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Messages analysés", "550")
+col2.metric("Clients à risque", "42")
+col3.metric("Score moyen satisfaction", "78%")
+
+st.subheader("Répartition des sentiments")
+
+st.bar_chart(df.set_index("Sentiment"))
+
+# Sélecteur
 sentiment = st.selectbox(
     "Choisir un sentiment",
-    ["Positif", "Neutre", "Négatif"]
+    df["Sentiment"]
 )
 
 st.write("Sentiment sélectionné :", sentiment)
