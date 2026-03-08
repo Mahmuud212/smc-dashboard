ec00aaec4964e71d8bd22df8da36926ecd3f7133
EL MANSOUR MAHMOUD <65395060+Mahmuud212@users.noreply.github.com>
2026-03-08 21:33:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.title("Dashboard IA - Satisfaction Client")
+
+st.write("Bienvenue dans le dashboard du projet SMC Insight.")
+
+sentiment = st.selectbox(
+    "Choisir un sentiment",
+    ["Positif", "Neutre", "Négatif"]
+)
+
+st.write("Sentiment sélectionné :", sentiment)
